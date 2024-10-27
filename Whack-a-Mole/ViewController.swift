6ef38cb59ab4855b775507e8e914163d36015d4e
DgknCtn <eem.dogukancetin@gmail.com>
2024-10-27 14:12:16 +0300

    Initial Commit

---
+++ Whack-a-Mole/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Whack-a-Mole
+//
+//  Created by Doğukan Çetin on 27.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
